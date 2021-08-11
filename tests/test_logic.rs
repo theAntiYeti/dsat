74079bf5b43d57eda83b68e502bd945b340e41b4
Dan Rastelli <rastellidani@gmail.com>
2021-08-11 16:42:23 +0100

    Add test for logic::Clause

--- tests/test_logic.rs
+++ tests/test_logic.rs
@@ -3,7 +3,7 @@ use dsat::logic;
 
 #[test]
 /* Test Clause */
-fn test_clause_assign() {
+fn test_clause_assign_true() {
     let test_clause = logic::Clause::Vars(vec![
         logic::Prop(true, 1),
         logic::Prop(true, 2),
@@ -11,4 +11,16 @@ fn test_clause_assign() {
     ]);
     let assigned = test_clause.assign(logic::Prop(true, 1));
     assert_eq!(assigned, logic::Clause::T);
+}
+
+#[test]
+fn test_clause_assign_false() {
+    let test_clause = logic::Clause::Vars(vec![
+        logic::Prop(false,1),
+        logic::Prop(false,2),
+    ]);
+    let assigned_1 = test_clause.assign(logic::Prop(true,1));
+    assert_eq!(assigned_1, logic::Clause::Vars(vec![logic::Prop(false, 2)]));
+    let assigned_2 = assigned_1.assign(logic::Prop(true,2));
+    assert_eq!(assigned_2, logic::Clause::F);
 }

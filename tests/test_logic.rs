6f47b5b9bd0f37c4d286c9f86318834e091c7f87
Dan Rastelli <rastellidani@gmail.com>
2021-08-11 16:43:22 +0100

    Formatting

--- tests/test_logic.rs
+++ tests/test_logic.rs
@@ -9,18 +9,17 @@ fn test_clause_assign_true() {
         logic::Prop(true, 2),
         logic::Prop(false, 3),
     ]);
+
     let assigned = test_clause.assign(logic::Prop(true, 1));
     assert_eq!(assigned, logic::Clause::T);
 }
 
 #[test]
 fn test_clause_assign_false() {
-    let test_clause = logic::Clause::Vars(vec![
-        logic::Prop(false,1),
-        logic::Prop(false,2),
-    ]);
-    let assigned_1 = test_clause.assign(logic::Prop(true,1));
+    let test_clause = logic::Clause::Vars(vec![logic::Prop(false, 1), logic::Prop(false, 2)]);
+
+    let assigned_1 = test_clause.assign(logic::Prop(true, 1));
     assert_eq!(assigned_1, logic::Clause::Vars(vec![logic::Prop(false, 2)]));
-    let assigned_2 = assigned_1.assign(logic::Prop(true,2));
+    let assigned_2 = assigned_1.assign(logic::Prop(true, 2));
     assert_eq!(assigned_2, logic::Clause::F);
 }

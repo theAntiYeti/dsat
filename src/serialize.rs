b597fc4322107e86a1e2d03be048ca8c47fb2692
Dan Rastelli <rastellidani@gmail.com>
2021-08-12 22:28:06 +0100

    Add docstrings to serialize.rs

--- src/serialize.rs
+++ src/serialize.rs
@@ -1,6 +1,6 @@
 use crate::logic;
-use logic::Prop;
 use logic::Clause;
+use logic::Prop;
 
 /// Serializes a proposition as a String.
 ///
@@ -14,6 +14,11 @@ pub fn serialize_prop(prop: &Prop) -> String {
     format!("{}{}", ex_indicator, prop.1)
 }
 
+/// Takes a string representation and parses as a Prop.
+///
+/// # Arguments
+///
+/// * `txt` - String to deserialize.
 pub fn deserialize_prop(txt: &str) -> Result<Prop, Box<dyn std::error::Error>> {
     let arity = txt.chars().next() != Some('!');
     let st: usize = if arity { 0 } else { 1 };
@@ -22,13 +27,23 @@ pub fn deserialize_prop(txt: &str) -> Result<Prop, Box<dyn std::error::Error>> {
     Ok(Prop(arity, value))
 }
 
+/// Takes an assignment (list of propositions) and serializes.
+///
+/// # Arguments
+///
+/// * `ass` - Vector representing the assignment.
 pub fn serialize_assignment(ass: &Vec<Prop>) -> String {
     let serialised_entries: Vec<String> = ass.iter().map(|x| serialize_prop(x)).collect();
     serialised_entries.join(",")
 }
 
+/// Takes a serialized clause and attempts to parse as a Clause.
+///
+/// # Arguments
+///
+/// * `txt` - String to deserialize.
 pub fn deserialize_clause(txt: &str) -> Result<Clause, Box<dyn std::error::Error>> {
-    let split = txt.split(","); 
+    let split = txt.split(",");
     let mut prop_list: Vec<Prop> = Vec::new();
 
     for part in split {

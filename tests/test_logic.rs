af0ad40c975ec2c85ca7ae8e8a6141f151275033
Dan Rastelli <rastellidani@gmail.com>
2021-08-12 14:51:03 +0100

    Add tests

--- tests/test_logic.rs
+++ tests/test_logic.rs
@@ -57,3 +57,38 @@ fn test_formula_mult_assign() {
         })
     );
 }
+
+#[test]
+fn test_formula_mult_assign_false() {
+    let test_formula = logic::Formula {
+        clauses: vec![
+            logic::Clause::Vars(vec![
+                logic::Prop(false, 1),
+                logic::Prop(false, 2),
+                logic::Prop(true, 3),
+            ]),
+            logic::Clause::Vars(vec![logic::Prop(true, 1)]),
+        ],
+    };
+
+    let assigned = test_formula.mult_assign(&vec![logic::Prop(false, 1)]);
+    assert_eq!(assigned, logic::Reduced::UNSAT);
+}
+
+#[test]
+fn test_get_unit() {
+    let test_formula = logic::Formula {
+        clauses: vec![
+            logic::Clause::Vars(vec![
+                logic::Prop(false, 1),
+                logic::Prop(false, 2),
+                logic::Prop(true, 3),
+            ]),
+            logic::Clause::Vars(vec![logic::Prop(true, 1)]),
+        ],
+    };
+
+    let unit_option = test_formula.get_unit();
+
+    assert_eq!(unit_option, Some(logic::Prop(true, 1)));
+}

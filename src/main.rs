e5506a6ea2fc8ed3b36cf0a35287675fca14ebb8
Dan Rastelli <rastellidani@gmail.com>
2021-08-13 20:16:07 +0100

    Create working API

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,39 @@
-#[macro_use] extern crate rocket;
+#![feature(decl_macro)]
+
+#[macro_use]
+extern crate rocket;
 
 mod dpll;
-use dsat::{dpll::dpll, serialize::{deserialize_formula, serialize_assignment}};
+mod logic;
+mod serialize;
+use dsat::{
+    dpll::dpll,
+    serialize::{deserialize_formula, serialize_assignment},
+};
 use std::str::FromStr;
 
 #[post("/solve", data = "<ser_formula>")]
-fn solve(ser_formula: &str) -> String {
-    let formula = deserialize_formula(ser_formula);
+fn solve(ser_formula: String) -> String {
+    let repped = ser_formula.replace("%2C", ",").replace("%26","&");
+    let split: Vec<&str> = repped.split("=").collect();
+    let translated = split[1];
+    let formula = deserialize_formula(translated);
 
     match formula {
-        Err(_) => return String::from_str("Parse error!").unwrap(),
+        Err(_) => return format!("Parse error: {}", translated),
         Ok(frm) => {
             let assignment = dpll(&frm);
             match assignment {
                 Some(ass) => return serialize_assignment(&ass),
                 None => return String::from_str("UNSAT").unwrap(),
             }
-        },
+        }
     }
 }
 
+fn main() {
 
+    let cors = rocket_cors::CorsOptions::default().to_cors().unwrap();
 
-#[launch]
-fn launch() -> _ {
-   rocket::build().mount("/", routes![solve]) 
+    rocket::ignite().attach(cors).mount("/", routes![solve]).launch();
 }

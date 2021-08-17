ab6729c6d4bca2c141cbf9f4e0cb9279670e85b6
Dan Rastelli <rastellidani@gmail.com>
2021-08-17 21:29:28 +0100

    Improve API, remove debug only feature

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 #![feature(decl_macro)]
 
+use std::io::Read;
+
 #[macro_use]
 extern crate rocket;
+use rocket::{Request, Data, Outcome, Outcome::*};
+use rocket::data::{self, FromDataSimple};
+use rocket::http::{Status, ContentType};
 
 mod dpll;
 mod logic;
@@ -12,13 +17,32 @@ use dsat::{
 };
 use std::str::FromStr;
 
+const LIMIT: u64 = 1000000;
+
+struct StringWrapper {
+    string: String,
+}
+
+impl FromDataSimple for StringWrapper {
+    type Error = String;
+
+    fn from_data(req: &Request, data: Data) -> data::Outcome<Self, String> {
+        let mut string = String::new();
+        if let Err(e) = data.open().take(LIMIT).read_to_string(&mut string) {
+            return Failure((Status::InternalServerError, format!("{:?}", e)));
+        }
+
+        return Success(StringWrapper{ string });
+    }
+}
+
 #[post("/solve", data = "<ser_formula>")]
-fn solve(ser_formula: String) -> String {
-    let repped = ser_formula.replace("%2C", ",").replace("%26","&").replace("%21", "!");
+fn solve(ser_formula: StringWrapper) -> String {
+    let string = ser_formula.string;
+    let repped = string.replace("%2C", ",").replace("%26","&").replace("%21", "!");
     let split: Vec<&str> = repped.split("=").collect();
     let translated = split[1];
     let formula = deserialize_formula(translated);
-    //println!("{}", translated);
 
     match formula {
         Err(_) => return format!("Parse error: {}", translated),
